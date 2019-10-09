8fc7df23ae80927c534008a14642edc233c01c7f
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2019-10-09 17:02:48 -0500

    feat: Make carousel thumbnails autoscroll

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,26 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+$('.carousel.thumbnail-indicator').on('slide.bs.carousel', function() {
+  var carousel = this;
+  setTimeout(function() {
+    // Get the thumbnails
+    var thumbnails = $(carousel).find(".carousel-indicators").children();
+    // Get the current thumbnail
+    var currentThumbnail = $(carousel).find(".carousel-indicators .active");
+    console.log(currentThumbnail);
+    // Get thumbnail's position
+    var activePos = currentThumbnail.offset().left;// + currentThumbnail.width()/2;
+
+    // Obtain the offset between the thumbnail and the center of container
+    var width = thumbnails.parent().width();
+    var maxScroll = thumbnails.parent()[0].scrollWidth - width;
+    var offset = activePos - width/2;
+    var scrollPos = thumbnails.parent().scrollLeft() + offset;
+    scrollPos = Math.max(Math.min(scrollPos, maxScroll), 0);
+    console.log(scrollPos);
+    thumbnails.parent().animate({scrollLeft: scrollPos}, 500);
+    //thumbnails.parent().scrollLeft(scrollPos);
+  }, 10);
+});
